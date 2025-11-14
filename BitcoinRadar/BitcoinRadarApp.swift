b37b21462d66eecc59d7872cd4f6ff2014606f3f
Hossein Asadi <hossein.asadi.p@gmail.com>
2025-11-14 11:41:33 +0100

    Initial Commit

---
+++ BitcoinRadar/BitcoinRadarApp.swift
@@ -0,0 +1,17 @@
+//
+//  BitcoinRadarApp.swift
+//  BitcoinRadar
+//
+//  Created by Hossein Asadiparchenaki on 14.11.25.
+//
+
+import SwiftUI
+
+@main
+struct BitcoinRadarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BitcoinRadarTests/BitcoinRadarTests.swift
@@ -0,0 +1,16 @@
+//
+//  BitcoinRadarTests.swift
+//  BitcoinRadarTests
+//
+//  Created by Hossein Asadiparchenaki on 14.11.25.
+//
+
+import Testing
+
+struct BitcoinRadarTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
